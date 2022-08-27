6cf87a1df04228ef1efa1a3c7265fe0fb204fa09
ewa1do <this.eduardovera@gmail.com>
2022-08-26 23:19:32 -0400

    adding list containing the pokemon types

---
+++ src/utilities/pokemonTypes.js
@@ -0,0 +1,18 @@
+export const pokemonTypes = [
+  'Bicho',
+  'Electrico',
+  'Fuego',
+  'Planta',
+  'Normal',
+  'Roca',
+  'Siniestro',
+  'Agua',
+  'Acero',
+  'Tierra',
+  'Lucha',
+  'Psiquico',
+  'Veneno',
+  'Dragon',
+  'Fantasma',
+  'Hada',
+];
