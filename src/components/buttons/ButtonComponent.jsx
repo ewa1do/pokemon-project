d557b5c2e4df0f7bf06f492fa4e9ecd744f7392c
ewa1do <this.eduardovera@gmail.com>
2022-08-29 18:43:03 -0400

    Refactoring ButtonComponent

--- src/components/buttons/ButtonComponent.jsx
+++ src/components/buttons/ButtonComponent.jsx
@@ -1,19 +1,28 @@
 import { Link } from 'react-router-dom';
+import Proptypes from 'prop-types';
+
+/**
+ * @param {Component} Icon A React Icon Component
+ * @param {string} value The button value
+ * @param {func} onClick Function for the button behavior
+ * @param {string} className Button style class
+ * @param {boolean} link Check if the button redirects to other page
+ * @param {string} path Path where you want the button to redirect
+ * @returns A functional React Component Button
+ */
 
 export const ButtonComponent = ({
   Icon,
   value,
   onClick,
-  type = '',
-  className = '',
-  link = false,
-  id,
+  className,
+  link,
+  path,
 }) => {
   const button = (
     <button
       className={className}
       onClick={onClick}
-      type={type}
     >
       <i>
         <Icon />
@@ -22,7 +31,22 @@ export const ButtonComponent = ({
     </button>
   );
 
-  const buttonLink = <Link to={`/pokemon/${id}`}>{button}</Link>;
+  const buttonLink = <Link to={path}>{button}</Link>;
 
   return link ? buttonLink : button;
+};
+
+ButtonComponent.defaultProps = {
+  className: 'button-active',
+  link: false,
+  value: '',
+};
+
+ButtonComponent.propTypes = {
+  Icon: Proptypes.func,
+  value: Proptypes.string,
+  onClick: Proptypes.func,
+  className: Proptypes.string,
+  link: Proptypes.bool,
+  path: Proptypes.string,
 };
