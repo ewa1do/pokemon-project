82b21370b466df2af8176bb6cf5fc8956765a858
ewa1do <this.eduardovera@gmail.com>
2022-09-01 20:05:30 -0400

    documenting component

--- src/components/lists/ListComponent.jsx
+++ src/components/lists/ListComponent.jsx
@@ -1,4 +1,12 @@
 import { v4 as uuid } from 'uuid';
+import Proptypes from 'prop-types';
+
+/**
+ *
+ * @param {array} list Array of elements to iterate
+ * @param {function} Component Component to be renderate on each iteration
+ * @returns A list of components
+ */
 
 export const ListComponent = ({ list, Component }) => {
   return list.map((item, i) => {
@@ -9,4 +17,9 @@ export const ListComponent = ({ list, Component }) => {
       />
     );
   });
+};
+
+ListComponent.propTypes = {
+  list: Proptypes.arrayOf(Proptypes.object).isRequired,
+  Component: Proptypes.func.isRequired,
 };
